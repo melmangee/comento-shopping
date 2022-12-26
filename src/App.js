92f5aa2ad6b9afb59227059fa07e4cd0cc26948a
melmangee <melmang@kakao.com>
2022-12-26 15:07:59 +0900

    feat : 네비게이션 컴포넌트 추가

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import './App.css';
+import Navigation from './components/Navigation';
 
 function App() {
-  return <div className="App">프로젝트 시작</div>;
+  return <div className="App">
+    <Navigation/>
+    코멘토 쇼핑
+    </div>;
 }
 
 export default App;
