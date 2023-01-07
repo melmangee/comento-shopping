b706d3acaa31ddd076de9d90dbfbeaf3540f84ed
melmangee <melmang@kakao.com>
2023-01-08 03:39:55 +0900

    feat: mock 샘플 데이터 추가

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,12 +2,13 @@ import Navigation from "../components/Navigation";
 import ThemeButton from "../components/ThemaButton";
 import ProductCard from "../components/ProductCard";
 import styled from "styled-components";
+import {mockTheme1Produdcts,mockTheme2Produdcts}from "../data/mockData"
+
 
 const Home = ()=> {
 
     return(
-        <div>
-            
+        <div> 
             <div>
             
             <Navigation NaviName={"코멘토 쇼핑"}/>
@@ -20,31 +21,18 @@ const Home = ()=> {
             </ThemeSection>
             
             <GrayLine2/>
-
             </div>
-            <div>
-                <ProductSection1>
-                <ProductCard name="비숑 블랙 머그잔" 
-                             description="쌀쌀한 날씨에 따뜻한 우유, 커피 한잔하기 좋은 
-                             블랙 & 화이트 비숑 머그잔입니다." 
-                             thumbnail="https://raw.githubusercontent.com/congchu/coment-shop-server/master/assets/images/product1.jpg"
-                />
-                </ProductSection1>
-                
-                <ProductSection2>
-                <ProductCard name="가열 보온 티코스터 온열 원터치 컵 받침대" 
-                             description="언제나 따뜻하게 최대 12시간 동안 최대 60도의 온도로 따듯한 차를 즐길 수 있습니다." 
-                             thumbnail="https://raw.githubusercontent.com/congchu/coment-shop-server/master/assets/images/product3.jpg"
-                /> 
-                </ProductSection2>
 
-                <ProductSection2>
-                <ProductCard name="벨루즈까사 솜사탕 파스텔 머그 4종 세트" 
-                             description="솜사탕처럼 부드러운 쉐입에 스트라이프 조각이 더해져 심플하면서도 세련된 파스텔 컬러의 머그" 
-                             thumbnail="https://raw.githubusercontent.com/congchu/coment-shop-server/master/assets/images/product2.jpg"
+            <productSection>
+             {/*TODO: mockData list를 화면에 노출하자*/}
+             {/*자바스크립트 map 문법*/}
+            {mockTheme1Produdcts.map((Product) =>( 
+                <ProductCard name={Product.name} 
+                             description={Product.description}
+                             thumbnail={Product.thumbnail}
                 />
-                </ProductSection2> 
-            </div>
+             ))}
+                </productSection>
         </div>
     );
 };
@@ -67,11 +55,8 @@ const GrayLine2 = styled.div`
  background: #EEEEEE;
 `;
 
-const ProductSection1 = styled.div`
+const ProductSection = styled.div`
  padding:43px 24px;
 `;
 
-const ProductSection2 = styled.div`
- padding:40px 24px;
-`;
 export default Home;
