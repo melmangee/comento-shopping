ffa1eede42b5b6d757ac33f4bb4320c700e17ac4
melmangee <melmang@kakao.com>
2023-01-10 01:02:50 +0900

    Update ProductDetail.jsx

--- src/pages/ProductDetail.jsx
+++ src/pages/ProductDetail.jsx
@@ -1,6 +1,10 @@
+import { useParams } from "react-router-dom";
 const ProductDetail = ()=> {
 
     return<div>상품상세 페이지</div>;
-    };
+    // URL에서 paramter 변수(productId) 받아오는 로직
+  let { productId } = useParams();
+
+};
     
     export default ProductDetail;
